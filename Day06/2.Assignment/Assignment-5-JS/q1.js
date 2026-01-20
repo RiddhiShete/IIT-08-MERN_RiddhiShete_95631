da782db7fdfb25a567b002ac03592465d2a2bfc9
RiddhiShete <riddhishete169@gmail.com>
2026-01-20 15:18:19 +0530

    Assignment 4 HTML

---
+++ Day06/2.Assignment/Assignment-5-JS/q1.js
@@ -0,0 +1,20 @@
+let a = 5;
+let s = "Hello";
+let b = true;
+let n = null;
+let u = undefined;
+const o = { 
+    'name': "Riddhi", 
+    'age': 21 
+}
+let arr=[1, 2, 3, 4, 5];
+
+console.log(`The data type of variable ${a} is: ${typeof a}`);
+console.log(`The data type of variable ${s} is: ${typeof s}`);
+console.log(`The data type of variable ${b} is: ${typeof b}`);
+console.log(`The data type of variable ${n} is: ${typeof n}`);
+console.log(`The data type of variable ${u} is: ${typeof u}`);
+console.log(`The data type of variable ${JSON.stringify(o)} is: ${typeof o}`);
+console.log(`The data type of variable ${arr} is: ${typeof arr}`);
+
+console.log(o);
